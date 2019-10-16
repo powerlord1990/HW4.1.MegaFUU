cc8afd349db45c9f148775c54d79b5188ced3774
powerlord1990 <fatik911turbo1@gmail.com>
2019-10-16 14:06:20 +0300

    Done

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -4,6 +4,16 @@ import ru.itpark.model.TurnOn;
 
 public class Main {
     public static void main(String[] args) {
-
+        TurnOn look = new TurnOn(
+                "Включайся. Смотри",
+                650,
+                30,
+                1_200,
+                true,
+                999_999,
+                300,
+                30,
+                "Кино и сериалы в Мегафон ТВ"
+        );
     }
 }
